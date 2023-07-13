135a88d39c4d8aec49e98a8719813557b341b9f2
imLulu8 <lucac8mmendatore@gmail.com>
2023-07-13 12:57:55 +0200

    feature():addExpensesComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ExpenseItem from './components/ExpenseItem/ExpenseItem';
-
+import Expense from './components/Expenses/Expenses';
 
 function App() {
 const expenses = [
@@ -27,26 +24,7 @@ const expenses = [
 
   return (
     <div className="App">
-      <ExpenseItem 
-      title={expenses[0].title} 
-      amount={expenses[0].amount} 
-      date={expenses[0].date}
-      />
-      <ExpenseItem
-       title={expenses[1].title} 
-       amount={expenses[1].amount} 
-       date={expenses[1].date}
-      />
-      <ExpenseItem
-       title={expenses[2].title} 
-       amount={expenses[2].amount} 
-       date={expenses[2].date}
-      />
-      <ExpenseItem
-       title={expenses[3].title} 
-       amount={expenses[3].amount} 
-       date={expenses[3].date}
-      />
+        <Expense items={expenses}/>
     </div>
   );
 }
